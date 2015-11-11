fbb875ec873a285376a14a6c1f2dab89ef72eefb
gowa66 <gowa66at@gmail.com>
2015-11-11 19:38:33 +0200

    asdqweasd

--- project/models.py
+++ project/models.py
@@ -31,12 +31,13 @@ class User(db.Model):
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String)
+    
     posts = relationship("BlogPost", backref="author")
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
-        self.password = bcrypt.generate_password_hash(password)
+        self.password = bcrypt.generate_password_hash(password.encode('utf-8'))
         
     def __repr__(self):
         return '<name {}'.format(self.name)
