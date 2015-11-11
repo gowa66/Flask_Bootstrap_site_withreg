3ce180f9b9957500a246cbcc2082e34c869abdaf
gowa66 <gowa66at@gmail.com>
2015-11-11 15:33:58 +0200

    123123

--- models.py
+++ models.py
@@ -29,7 +29,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.String)
     posts = relationship("BlogPost", backref="author")
 
     def __init__(self, name, email, password):
