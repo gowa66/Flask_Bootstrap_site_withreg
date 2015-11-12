8cbab7cef4e430b9aedf99e079c35f89adce4961
gowa66 <gowa66at@gmail.com>
2015-11-12 20:08:53 +0200

    finish

--- project/home/views.py
+++ project/home/views.py
@@ -1,11 +1,12 @@
 #################
 #### imports ####
 #################
-from project import app, db
+from project import db
 from project.models import BlogPost
+from .forms import MessageForm
+from flask import render_template, Blueprint, url_for, redirect, request
+from flask.ext.login import login_required, current_user
 
-from flask import flash, redirect, session, url_for, render_template, Blueprint
-from functools import wraps
  
 ################
 #### config ####
@@ -17,19 +18,7 @@ home_blueprint = Blueprint(
 )
 
 
-##########################
-#### helper functions ####
-##########################
 
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            return redirect(url_for('users.login'))
-    return wrap
 
 
 ################
@@ -37,12 +26,25 @@ def login_required(test):
 ################
 
 # use decorators to link the function to a url
-@home_blueprint.route('/')
-@login_required
+@home_blueprint.route('/', methods=['GET', 'POST'])   # pragma: no cover
+@login_required   # pragma: no cover
 def home():
-    # return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
-    return render_template('home.html', posts=posts)  # render a template
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template(
+            'home.html', posts=posts, form=form, error=error)
 
 
 
--- project/models.py
+++ project/models.py
@@ -15,9 +15,10 @@ class BlogPost(db.Model):
     description = db.Column(db.String, nullable=False)
     author_id = db.Column(db.Integer, ForeignKey('users.id'))
     
-    def __init__(self, title, description):
+    def __init__(self, title, description, author_id):
         self.title = title
         self.description = description
+        self.author_id = author_id
 
     def __repr__(self):
         return '<title {}'.format(self.title)
@@ -38,6 +39,19 @@ class User(db.Model):
         self.name = name
         self.email = email
         self.password = bcrypt.generate_password_hash(password.encode('utf-8'))
+    
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)  
         
+              
     def __repr__(self):
         return '<name {}'.format(self.name)
